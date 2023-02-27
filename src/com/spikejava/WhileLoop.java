b836d1f79685e7536d16528d0a15089effe0fc2c
alejandro <alexcs9230@gmail.com>
2023-02-27 12:55:41 +0100

    Bucles while

---
+++ src/com/spikejava/WhileLoop.java
@@ -0,0 +1,16 @@
+package com.spikejava;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+
+
+        int count = 0;
+        while(count < 10){
+
+            System.out.println("Hola mundo " + count );
+            count ++; // condicion
+
+        }
+        System.out.println("Fin");
+    }
+}
