4fbeb5b7362fadb68db48b417a745abe3b068d4f
alejandro <alexcs9230@gmail.com>
2023-02-27 13:59:03 +0100

    Uso de continue y break

--- src/com/spikejava/WhileLoop.java
+++ src/com/spikejava/WhileLoop.java
@@ -6,9 +6,13 @@ public class WhileLoop {
 
         int count = 0;
         while(count < 10){
+            count ++; // condicion
+            if(count == 6)
+                break; // rompe el flujo de ejecucion
+                //continue; // salta el valor 6 y continua a la siguiente iteracion
 
             System.out.println("Hola mundo " + count );
-            count ++; // condicion
+
 
         }
         System.out.println("Fin");
