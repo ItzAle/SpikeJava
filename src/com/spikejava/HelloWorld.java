c965bc0517915f2684c29f75928d66ee58a74114
alejandro <alexcs9230@gmail.com>
2023-02-27 11:10:12 +0100

    Comentarios y Documentacion

--- src/com/spikejava/HelloWorld.java
+++ src/com/spikejava/HelloWorld.java
@@ -1,10 +1,30 @@
 package com.spikejava;
 
+/**
+ * Clase principal punto de entrada a aplicacion
+ *
+ * @author ale
+ * @see ByeWorld
+ */
 public class HelloWorld {
-
+    /**
+     * Funcion principal punto de entrada a la aplicacion
+     * @param args argumentos a recibr
+     * @since   1.0
+     */
     public static void main(String[] args){
 
 
-        System.out.println("Hola mundo desde clase HelloWorld");
+        // Comentario
+        /* Aqui un texto de comentario */
+
+        /*
+        1.Abrir Fichero
+        2. Leer Fichero
+        3. Procesar fichero
+        4. Cerrar Fichero
+         */
+        System.out.println("Hola mundo desde clase HelloWorld"); // Imprimir texto
+
     }
 }
