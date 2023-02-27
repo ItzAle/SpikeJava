9cb0b985017c07af0f348562d4f50ba54fac16f2
alejandro <alexcs9230@gmail.com>
2023-02-27 11:26:39 +0100

    Operadores

---
+++ src/com/spikejava/Operadores.java
@@ -0,0 +1,53 @@
+package com.spikejava;
+
+public class Operadores {
+    public static void main(String[] args) {
+
+
+        // Operadores
+
+        // aritmeticos
+
+        // + - / * %
+        int number1 = 5;
+        int number2 = 2;
+
+        int resultado = number1 + number2;
+        int resultado2 = number1 - number2;
+        int resultado3 = number1 * number2;
+        int resultado4 = number1 / number2;
+
+
+        // logicos, relacion, comparacionm, booleanos
+        /*
+        >
+        >=
+        <
+        <=
+        ==
+        !=
+        && and
+        || or
+        !
+         */
+
+        // asignacion
+        /*
+        =
+        +=
+        -=
+        /=
+        *=
+        %=
+         */
+
+        // incremento
+        // ++
+
+        // decremento
+        // --
+
+        //concatenacion
+        // +
+    }
+}
