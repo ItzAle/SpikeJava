d5df391ce81c107e7e2f059e03b957ec2278f52c
alejandro <alexcs9230@gmail.com>
2023-02-27 11:17:09 +0100

    Tipos de datos

---
+++ src/com/spikejava/Tipos.java
@@ -0,0 +1,39 @@
+package com.spikejava;
+
+public class Tipos {
+
+    public static void main(String[] args) {
+
+
+        // Esquema
+        // tipo identificador = 30;
+        // tipo identificador;
+        // identificador = 30;
+
+        //enteros
+        byte number1 = 1; // 1 Byte
+        short number2 = 2; // 2 Byte
+        int number3 = 3; // 4 Byte
+        long number4 = 4; // 8 Byte
+
+        // punto flotante
+        float decimal1 = 4.9f;
+        double decimal2 = 9.99d;
+
+        // caracter
+        char caracter1 = 'a';
+
+        //booleanos
+        boolean verdadero = true;
+        boolean falso = false;
+
+        // cadenas de texto
+        String nombre = "Alejandro";
+        String apellido = "Caballero";
+
+        //tipos envoltorio
+
+        Integer numero = null;
+        Long numero2 = 2L;
+    }
+}
