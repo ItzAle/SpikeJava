107c4e3d1e7d8a5adbfcaa1761325db062583fe7
alejandro <alexcs9230@gmail.com>
2023-03-01 10:14:01 +0100

    Crear un objeto

--- src/com/spikejava/poo/clases/Main.java
+++ src/com/spikejava/poo/clases/Main.java
@@ -3,6 +3,20 @@ package com.spikejava.poo.clases;
 public class Main {
     public static void main(String[] args) {
 
+        // Clase identificador = new Clase ();
+
+
+        // Crear un objeto utilizando el constructor vacio
+        Vehiculo toyotaPrius = new Vehiculo();
+
+        // crear un objeto utilizando el constructor con parametros
+
+        Vehiculo toyotaYaris = new Vehiculo("Toyota", "Yaris", 2.5,2019, "azul", true);
+        System.out.println(toyotaYaris.fabricante);
+        System.out.println(toyotaYaris.year);
+        System.out.println(toyotaYaris.speed);
+        toyotaYaris.acelerar(50);
+        System.out.println(toyotaYaris.speed);
 
 
     }
