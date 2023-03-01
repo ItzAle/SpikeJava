18dd0c4976412e2bf7cb3c8b046a2e051a444598
alejandro <alexcs9230@gmail.com>
2023-03-01 09:20:49 +0100

    Constructores

--- src/com/spikejava/poo/clases/Main.java
+++ src/com/spikejava/poo/clases/Main.java
@@ -3,5 +3,7 @@ package com.spikejava.poo.clases;
 public class Main {
     public static void main(String[] args) {
 
+
+
     }
 }
---
+++ src/com/spikejava/poo/clases/Vehiculo.java
@@ -0,0 +1,17 @@
+package com.spikejava.poo.clases;
+
+public class Vehiculo {
+    // 1. Atributos
+    String fabricante;
+    String modelo;
+    Double cc;
+    int year;
+    String color;
+    boolean sport;
+
+
+
+    // 2. Constructores
+
+    // 3. Metodos (comportamiento)
+}
