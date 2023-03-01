81d93b4c16504e551acafb1aa7a67b6adb94499c
alejandro <alexcs9230@gmail.com>
2023-03-01 09:28:09 +0100

    Constructores 2

--- src/com/spikejava/poo/clases/Vehiculo.java
+++ src/com/spikejava/poo/clases/Vehiculo.java
@@ -9,9 +9,29 @@ public class Vehiculo {
     String color;
     boolean sport;
 
-
-
     // 2. Constructores
+    public Vehiculo(){
+
+    }
+    public Vehiculo(String fabricante, String modelo, Double cc, int year, String color, boolean sport){
+
+        this.fabricante = fabricante;
+        this.modelo = modelo;
+        this.cc = cc;
+        this.year = year;
+        this.color = color;
+        this.sport = sport;
+
+    }
+    public Vehiculo(String fabricante, String modelo){
+
+        this.fabricante = fabricante;
+        this.modelo = modelo;
+
+    }
+
+
+
 
     // 3. Metodos (comportamiento)
 }
